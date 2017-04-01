16fd0d382a7c9304553cea52be3e27997fc4008b
Kha Nguyen <kha.vng@gmail.com>
2017-04-01 01:28:17 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,76 @@
+// Show viewport height
+  // -----------
+  // Debugger that shows view port size. Helps when making responsive designs.
+  // -----------
+  function showViewPortSize(display) {
+    if(display) {
+      var height = window.innerHeight;
+      var width = window.innerWidth;
+      jQuery('body').prepend('<div id="viewportsize" style="z-index:9999;position:fixed;bottom:0px;left:0px;color:#fff;background:#000;padding:10px">Height: '+height+'<br>Width: '+width+'</div>');
+      jQuery(window).resize(function() {
+              height = window.innerHeight;
+              width = window.innerWidth;
+              jQuery('#viewportsize').html('Height: '+height+'<br>Width: '+width);
+      });
+    }
+  }
+
+  $(document).ready(function(){
+     showViewPortSize(true);
+  });
+
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['intro', 'student', 'projects', 'hobbies', 'contact'],
+		verticalCentered: false,
+		css3: true, 
+		responsiveHeight: 550,
+		responsiveWidth: 1000,
+
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['Intro', 'Student', 'Projects', 'Hobbies', 'Contact'],
+		// Slides
+		slidesNavigation: true,
+		slidesNavPosition: 'bottom',
+		controlArrows: false
+	});
+});
+
+// $("#introduction").typeIt({
+// 	strings: ['Hi, my name is Kha.', 'I am a student.', 'I am a programmer.', 'I am a musician.',' I am a hobbyist.']
+// });
+
+$('#introduction').typeIt({
+})
+	.tiType('Hi, my name is Kha.')
+	.tiBreak()
+	.tiType('I am a ')
+	.tiType("<a href='#student'>student</a>.")
+	.tiPause(1500)
+// .tiDelete(8)
+	.tiBreak()
+	.tiType('I am a ')
+	.tiType("<a href='#projects'>programmer</a>.")
+
+// Image Slideshows
+$(document).ready(function () {
+	//initialize swiper when document ready  
+	var mySwiper = new Swiper ('.swiper-container', {
+		// Optional parameters
+		direction: 'horizontal',
+		mode: 'horizontal',
+		loop: true,
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		autoplay: 5000,
+		autoplayDisableOnInteraction: true,
+		spaceBetween: 10,
+	})        
+	});
+
+// Card flip
+$('.flip-container').click(function (e) {
+	$(this).toggleClass('flipped');
+});
